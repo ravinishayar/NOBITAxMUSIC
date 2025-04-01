e4ddd7b6313512c9913e28a1b15a1c7f761c6e29
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-04-01 22:50:44 +0530

    Update wishcute.py

--- SONALI/plugins/yumi/wishcute.py
+++ SONALI/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI import app 
 
-SUPPORT_CHAT = "PURVI_SUPPORT"
+SUPPORT_CHAT = "OG_DEFAULTERS_001"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
